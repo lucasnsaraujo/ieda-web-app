c743742be356e626054287c4bca53c10675acefa
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2024-07-26 16:54:48 -0300

    creating a mock first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1>This is the web app</h1>
+      <div className="w-full h-screen flex justify-center items-center bg-green-100">
+        <Image
+          src={"/logo-ieda-green.png"}
+          width={1200}
+          height={1200}
+          className="w-72 md:w-80 lg:w-96 "
+          alt="Logo IEDA"
+        />
+
+        <Image
+          src={"/ieda-logo.png"}
+          width={1200}
+          height={1200}
+          className="w-72 md:w-80 lg:w-96 absolute hover:opacity-0 transition-opacity"
+          alt="Logo IEDA"
+        />
+      </div>
     </>
   );
 }
